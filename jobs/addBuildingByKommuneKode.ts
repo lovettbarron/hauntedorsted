fe6134262b06d1dc5ecd4f7b6020be1c9c7cb1ad
Andrew Lovett-Barron <alb@andrewlb.com>
2023-09-29 00:10:40 +0200

    Updated the  build job

--- jobs/addBuildingByKommuneKode.ts
+++ jobs/addBuildingByKommuneKode.ts
@@ -1,6 +1,14 @@
-import { AddBuildingByKommuneKode } from '@teamkeel/sdk';
+import { AddBuildingByKommuneKode, models } from '@teamkeel/sdk';
 import fetch from "node-fetch";
+import { parse } from 'csv-parse/sync';
 
+function findByMatchingProperties(set, properties) {
+    return set.filter(function (entry) {
+        return Object.keys(properties).every(function (key) {
+            return entry[key] === properties[key];
+        });
+    });
+}
 
 // To learn more about jobs, visit https://docs.keel.so/jobs
 export default AddBuildingByKommuneKode(async (ctx, inputs) => {
@@ -21,7 +29,27 @@ export default AddBuildingByKommuneKode(async (ctx, inputs) => {
 	console.log(res)
 
 	if(res && res.length > 1) {
+        // Sync method might not work given data sets are several 100mb.
+        // Should prob use the steam api https://csv.js.org/parse/api/stream/
+        const rec = parse(res, {
+            columns: true,
+            skip_empty_lines: true
+          })
+          .reduce( (acc,val) => {
 
-    }
+                const addr = {
+                    street: val.vejnavn,
+                    number: val.husnr,
+                    post: val.postnr
+                }
 
+                if(!findByMatchingProperties(acc,addr)) acc.push(addr)
+                
+                return acc
+            })  
+
+            for (const address in rec) {
+                models.building.create(address)
+            }
+        }
 });
